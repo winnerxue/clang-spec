77ad846cf203ee901bca6f03cc09f67b075ebed6
winnerxue <winnerxue.xz@gmail.com>
2022-03-13 11:50:20 +0800

    iso.init

--- helloworld/main.c
+++ helloworld/main.c
@@ -1,71 +1,7 @@
 #include<stdio.h>
 
-// 1. 宏定义
-#define PI 3.1415926
-
-// 2. 宏函数(由于宏在编译器会自动替换，所以ADD函数替换最好加一层括号)
-#define ADD(a,b) ((a)+(b))
-#define MAX(a,b) ((a)>(b)?(a):(b))
-
-// 3. 多行宏定义
-#define PRINTARR(arr,n) \
-	char* format=""; \
-	unsigned char space=sizeof(arr)/n;\
-	if(space==2 || space==1) {\
-		format="%d "; \
-	}else if(space==4 || space==8){\
-		format="%.2f "; \
-	}\
-	for(unsigned int i=0;i<n;i++) {\
-		printf(format,arr[i]); \
-	}
-
-// 4. 条件编译1(注意，在预编译后，宏会替换为空，所以，不应该使用宏作为变量使用)
-#define C_LANG_RUNTIME
-#define CPP_LANG_RUNTIME
-
-#ifndef C_LANG_RUNTIME	
-#define COMPILE="C"
-#endif // !C_LANG_RUNTIME
-
-#ifndef CPP_LANG_RUNTIME
-#define COMPILE="CPP"
-#endif // !CPP_LANG_RUNTIME
-
-
-// 5. 条件编译2(头文件中避免重复定义)
-#ifndef HTTP_H
-#define HTTP_H
-
-#endif // !HTTP_H
-
-// 6. 预定义的内置宏（依赖于C标准和指定的平台）[未全]
-#define NOW()  __TIME__ 
-#define DATE()  __DATE__
-#define FILENAME()  __FILE__ 
-#define LINE()  __LINE__ 
-
-
-// 宏常见片段
-
-// (1) 字段在结构体中的偏移量
-#define OFFSETOF(type,field) ((size_t)&(((type*)0)->field))
-
-// (2) 结构体中字段所占用的字节
-#define FIELDSIZE(type,field) sizeof(((type*)0)->field)
-
-// (3) 大小写互相转换
-#define TO_UPPER_OR_LETTER(c) ((c)^32)
-
-// (4) 数组中元素个数
-#define SIZE_OF_ARRAY(arr) (sizeof((arr))/sizeof((arr[0])))
-
-// (5) 数据大小比较
-#define MIN(a,b) ((a)<(b)?(a):(b))
-#define MAX(a,b) ((a)>(b)?(a):(b))
-
-
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-	return 0;
+	printf("helloworld");
+	return;
 }
